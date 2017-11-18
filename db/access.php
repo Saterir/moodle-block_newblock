2895d4ba82fe68a4a53f319bbfaa3a7cfb9982fc
Saterir <locuragod@gmail.com>
2017-11-18 09:40:31 -0300

    Creado la capacidad en el bloque para el rol Librarian

--- db/access.php
+++ db/access.php
@@ -26,11 +26,11 @@ defined('MOODLE_INTERNAL') || die();
 
 $capabilities = array(
 
-    'block/newblock:myaddinstance' => array(
-        'captype' => 'write',
-        'contextlevel' => CONTEXT_SYSTEM,
-        'archetypes' => array(
-            'user' => CAP_ALLOW
+    'block/newblock:Librarian' => array(
+        'captype'      => 'write',
+    	'contextlevel' => CONTEXT_MODULE,
+        'archetypes'   => array(
+        	'student'    => CAP_ALLOW
         ),
 
         'clonepermissionsfrom' => 'moodle/my:manageblocks'
